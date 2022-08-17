d05783552a909a12bb4a4b4083cddaa43a0bc02a
Yeliz <81921661+yelizsn@users.noreply.github.com>
2022-08-17 12:20:30 +0300

    Add files via upload

---
+++ DataViewOrnek3 (Giriş Ekranı)/DataViewOrnek3/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace DataViewOrnek3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kadi = new System.Windows.Forms.TextBox();
+            this.sifre = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // kadi
+            // 
+            this.kadi.Location = new System.Drawing.Point(12, 25);
+            this.kadi.Name = "kadi";
+            this.kadi.Size = new System.Drawing.Size(160, 20);
+            this.kadi.TabIndex = 0;
+            // 
+            // sifre
+            // 
+            this.sifre.Location = new System.Drawing.Point(12, 64);
+            this.sifre.Name = "sifre";
+            this.sifre.PasswordChar = '*';
+            this.sifre.Size = new System.Drawing.Size(160, 20);
+            this.sifre.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 59);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Giriş Yap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kullanıcı Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Şifre";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(259, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 59);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Kayıt Ol";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 97);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sifre);
+            this.Controls.Add(this.kadi);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Giriş Ekranı";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox kadi;
+        private System.Windows.Forms.TextBox sifre;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DataViewOrnek3 (Giriş Ekranı)/DataViewOrnek3/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataViewOrnek3
+{
+    public partial class Form1 : Form
+    {
+        OleDbDataAdapter da;
+        OleDbCommand cmd;
+        DataSet ds;
+        DataView dataView;
+        string ConnectionString = "Provider=Microsoft.ACE.Oledb.12.0;Data Source=kullanici.accdb";
+        public Form1()
+        {
+            InitializeComponent();
+            Veri_Cek();
+        }
+        void Veri_Cek()
+        {
+            using (OleDbConnection baglanti = new OleDbConnection(ConnectionString))
+            {
+
+                dataView = new DataView();
+                ds = new DataSet();
+                cmd = new OleDbCommand("SELECT * FROM Kullanici", baglanti);
+                da = new OleDbDataAdapter();
+                da.SelectCommand = cmd;
+                da.Fill(ds, "Add New");
+                dataView = new DataView(ds.Tables[0]);
+                dataView.Sort = "Id";
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataView.Count; i++)
+            {
+                if (kadi.Text == dataView[i][1].ToString() && sifre.Text == dataView[i][2].ToString())
+                {
+                    MessageBox.Show("Merhaba, " + dataView[i][1].ToString() + ". Başarıyla giriş yaptınız.");
+                    return;
+                }
+            }
+            MessageBox.Show("Kullanici adınız veya şifreniz yanlış. Lütfen tekrar deneyiniz.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Kullanici_Ekle(kadi.Text, sifre.Text);
+        }
+
+        void Kullanici_Ekle(string kadi, string sifre)
+        {
+            try
+            {
+                DataRowView newRow = dataView.AddNew();
+                newRow["Kullanici_Adi"] = kadi;
+                newRow["Sifre"] = sifre;
+                newRow.EndEdit();
+                dataView.Sort = "Id";
+
+                using (OleDbConnection baglanti = new OleDbConnection(ConnectionString))
+                {
+                    OleDbCommand com = new OleDbCommand("INSERT INTO Kullanici (Kullanici_Adi,Sifre) VALUES (@Kullanici_Adi,@Sifre)");
+                    com.Connection = baglanti;
+                    baglanti.Open();
+                    if (baglanti.State == ConnectionState.Open)
+                    {
+                        com.Parameters.Add("@Kullanici_Adi", OleDbType.VarChar).Value = dataView[0][1];
+                        com.Parameters.Add("@Sifre", OleDbType.VarChar).Value = dataView[0][2];
+                        try
+                        {            
+                            com.ExecuteNonQuery();
+                            MessageBox.Show("Başarıyla kayıt oldunuz.");
+                            Veri_Cek();
+                        }
+                        catch (OleDbException ex)
+                        {
+                            MessageBox.Show(ex.Source);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bağlantı Hatası");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
